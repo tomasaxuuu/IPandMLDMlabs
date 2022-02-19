d0fbcbca4d099c795c577a6614d90aee3ad29fda
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-02-20 01:52:20 +0400

    Доделал основную функцию

--- MLDM labs/Lab1/Scripts/1scripts.js
+++ MLDM labs/Lab1/Scripts/1scripts.js
@@ -1,22 +1,23 @@
 var error_message = "";
-// Функция валидации (проверка логического ввода матриц)
 
-function qvalidate(mes) {
+// Функция валидации (проверка логического ввода матриц)
+function Validation(mes) {
     let mass = true;
     if (mes.length > 0) {
         mass = mes.split(" ");
+        // проверка на валидацию
         for (let i = 0; i < mass.length; i++){
             if (mass[i].length != 3) {
                 error_message = 'Размер элемента должен быть равен 3 символам!';
                 mass = false;
                 break;
             }
-            if(!(mass[i][0] < 'A' || mass[i][0] > 'z')) {
+            if (!(mass[i][0] < 'A' || mass[i][0] > 'z')) {
                 error_message = 'Первый элемент должен быть цифрой!';
                 mass = false;
                 break;
             }
-            if(!(mass[i][0] < 'A' || mass[i][0] > 'z')) {
+            if (!(mass[i][0] < 'A' || mass[i][0] > 'z')) {
                 error_message = 'Второй элемент должен быть цифрой!';
                 mass = false;
                 break;
@@ -39,15 +40,27 @@ function qvalidate(mes) {
         return mass;
     }
 
-// Основаная функция для подсчета
-
-function myFunc() {
+// Основаная функция для работы с множествами и выполнением действий
+function Sets() {
     var mas_1 = document.getElementById('matrix1');
     var mas_2 = document.getElementById('matrix2');
-    if ((qvalidate(mas_1.value)) == false) {
-        alert(error_message);
-    }
-    if ((qvalidate(mas_2.value)) == false) {
+    if (Validation(mas_1.value) && Validation(mas_2.value)) { // true
+        const mas_1_set = new Set(mas_1.value.split(" "));
+        const mas_2_set = new Set(mas_2.value.split(" ")); // создание двух множеств
+        let union = [...new Set([...mas_1_set, ...mas_2_set])]; // объеденение двух множеств
+        let intersection = (mas_1.value.split(" ")).filter(x => (mas_2.value.split(" ")).includes(x)); // пересечение двух множеств
+        let difference_one = (mas_1.value.split(" ")).filter(x => !mas_2.value.split(" ").includes(x)); // дополнение A\B
+        let difference_two= (mas_2.value.split(" ")).filter(x => !mas_1.value.split(" ").includes(x)); // дополнение B\A
+        let symmetrical_difference = mas_1.value.split(" ").filter(x => !mas_2.value.split(" ").includes(x)).concat(mas_2.value.split(" ").filter(x => !mas_1.value.split(" ").includes(x))); // симметричная разность двух множеств
+        // вывод результатов в документ при условии true
+        document.getElementById('union').innerHTML = [...union.values()];
+        document.getElementById('intersection').innerHTML = [...intersection.values()];
+        document.getElementById('difference_AB').innerHTML = [...difference_one.values()];
+        document.getElementById('difference_BA').innerHTML = [...difference_two.values()];
+        document.getElementById('symmetrical_difference').innerHTML = [...symmetrical_difference.values()];
+    }  
+    // false - вывод ошибок
+    else {
         alert(error_message);
     }
 }
