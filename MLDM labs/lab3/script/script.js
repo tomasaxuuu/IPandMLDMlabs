d5aef3e3a747c89f53f3ec5861ec278e4417a42d
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-04-03 01:46:54 +0400

    final version

--- MLDM labs/lab3/script/script.js
+++ MLDM labs/lab3/script/script.js
@@ -6,21 +6,25 @@ function validation(firstSet, secondSet, relation) {
     let rel = relation;
     let valid = true;
     if(firstSet.length > 0 && secondSet.length > 0 && relation.length > 0) {
+        //проверка длины множеств
         if(a.length != 4 || b.length != 4) {
             error_message = "Размер множеств должен быть равен четырём элементам!"
             valid = false;
         }
         for(let i = 0; i < rel.length; i++) {
+            //проверка длины множества отношений
             if(rel.length != 4) {
-                error_message = "Размер матрицы отношения должен иметь длину А + B!"
+                error_message = "Размер множества отношения должен иметь длину А + B!"
                 valid = false;
                 break;
             }
+            // проверка размерности каждого элемента
             if(rel[i].length != 2) {
                 error_message = "Размер элемента должен быть равен 2!"
                 valid = false;
                 break;
             }
+            // проверка первого на принадлежность к множеству А
             switch (rel[i][0]) {
                 case a[0]:
                 case a[1]:
@@ -32,6 +36,7 @@ function validation(firstSet, secondSet, relation) {
                 valid = false;
                 error_message = "Не хватает элементов множества А!";
             }
+            // проверка второго на приадлежность к множеству B
             switch (rel[i][1]) {
                 case b[0]:
                 case b[1]:
@@ -57,6 +62,7 @@ function rel() {
     let setA = document.getElementById('firstSet').value;
     let setB = document.getElementById('secondSet').value;
     let relat = document.getElementById('relationships').value.split(',');
+    // массив из 0 для записи в него матрицы отношений
     let resultArray = [
         [0, 0, 0, 0], 
         [0, 0, 0, 0], 
@@ -68,20 +74,25 @@ function rel() {
     if (validation(setA, setB, relat)) {
         setA = setA.split(' ');
         setB = setB.split(' ');
+        // построение матрицы отношений
         for (let i = 0; i < relat.length; i++){
+            // проверка каждого элемента из матрицы отношений на наличие в множествах A и B
             if (Array.from(setA).indexOf(relat[i][0]) >= 0 && Array.from(setB).indexOf(relat[i][1]) >= 0) {
+                // ставим 1 на пересечении
                 resultArray[Array.from(setA).indexOf(relat[i][0])][Array.from(setB).indexOf(relat[i][1])] = 1;
             }
         }
+        // подсчет суммы в строках для определения функций
         let sumA = 0;
         let sumB = 0;
         for (let i = 0; i < resultArray.length; i++){
             sumA = 0;
             sumB = 0;
-            for (let j = 0; j < resultArray[0].length; j++){
+            for (let j = 0; j < resultArray[i].length; j++){
                 sumA += resultArray[i][j];
                 sumB += resultArray[j][i];
             }
+            // если сумма != 1, то это не функция
             if (sumA != 1) {
                 finalA = false;
             }
@@ -89,9 +100,11 @@ function rel() {
                 finalB = false;
             }
         }
+        // вывод матрицы отношений
         for (let i = 1; i < resultArray.length + 1; i++){
             document.getElementById('results' + i).innerHTML = resultArray[i - 1].join('');
         }
+        // вывод сообщений о функциях
         if (finalA) {
             document.getElementById('AB').innerHTML = "Отношение является функцией (A к B)";
         }
