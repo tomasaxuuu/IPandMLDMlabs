a706af1cbf688e78a821922efffe0369697b1101
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-04-19 20:05:08 +0400

    Изменил все страницы на .PHP

--- IPlabs/pages/Aboutme.php
+++ IPlabs/pages/Aboutme.php
@@ -15,9 +15,9 @@
             <div class="link-container">
                 <div class="pages">
                     <a class="buts" href="../index.html">Home</a>
-                    <a class="buts" href="../pages/Aboutme.html">About me</a>
-                    <a class="buts" href="../pages/Hobbies.html">Hobbies</a>
-                    <a class="buts" href="../pages/Gallery.html">Gallery</a>
+                    <a class="buts" href="../pages/Aboutme.php">About me</a>
+                    <a class="buts" href="../pages/Hobbies.php">Hobbies</a>
+                    <a class="buts" href="../pages/Gallery.php">Gallery</a>
                     <a class="buts" href="../template/auth.php">Auth</a>
                 </div>
                 <div class="soc">
@@ -25,7 +25,7 @@
                     <a class="social" target="blank" href="https://vk.com/tomasaxuuu"><img src="../IMG/vk.png" width="25" height="25"></a>
                     <a class="social" target="blank" href="https://open.spotify.com/user/5uh6ik7nbma6wbpaiq9ljr3i9"><img src="../IMG/spoty.png" width="25" height="25"></a>
                 </div>
-                <div class="url"><a href="../pages/Links.html">Links</a></div>
+                <div class="url"><a href="../pages/Links.php">Links</a></div>
             </div>
         </header>
     </div>
--- IPlabs/template/registr.php
+++ IPlabs/template/registr.php
@@ -12,9 +12,9 @@
             <div class="link-container">
                 <div class="pages">
                     <a class="buts" href="../index.html">Home</a>
-                    <a class="buts" href="../pages/Aboutme.html">About me</a>
-                    <a class="buts" href="../pages/Hobbies.html">Hobbies</a>
-                    <a class="buts" href="../pages/Gallery.html">Gallery</a>
+                    <a class="buts" href="../pages/Aboutme.php">About me</a>
+                    <a class="buts" href="../pages/Hobbies.php">Hobbies</a>
+                    <a class="buts" href="../pages/Gallery.php">Gallery</a>
                     <a class="buts" href="#">Registr</a>
                 </div>
                 <div class="soc">
@@ -22,7 +22,7 @@
                     <a class="social" target="blank" href="https://vk.com/tomasaxuuu"><img src="../IMG/vk.png" width="25" height="25"></a>
                     <a class="social" target="blank" href="https://open.spotify.com/user/5uh6ik7nbma6wbpaiq9ljr3i9"><img src="../IMG/spoty.png" width="25" height="25"></a>
                 </div>
-                <div class="url"><a href="../pages/Links.html">Links</a></div>
+                <div class="url"><a href="../pages/Links.php">Links</a></div>
             </div>
         </header>
     </div>
