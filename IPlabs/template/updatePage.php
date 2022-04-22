f9b5401d5abc193df734a693d6494c22a8172dcd
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-04-23 01:31:44 +0400

    Сделал авторизацию, регистрацию, таблицу пользователей + удаление + редакт

--- IPlabs/template/updatePage.php
+++ IPlabs/template/updatePage.php
@@ -10,9 +10,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/updatePage.css">
     <title>Update</title>
 </head>
 <body>
+    <div class="fixed">
+        <header id="header">
+            <a class="logo" href="#">Edit</a>
+            <div class="burger"><img src="../IMG/menu.png"></div>
+                <div class="link-container">
+                    <div class="pages">
+                        <a class="buts" href="../index.html">Home</a>
+                        <a class="buts" href="../pages/Aboutme.php">About me</a>
+                        <a class="buts" href="../pages/Hobbies.php">Hobbies</a>
+                        <a class="buts" href="../pages/Gallery.php">Gallery</a>
+                        <a class="buts" href="../template/profile.php">Profile</a>
+                    </div>
+                    <div class="soc">
+                        <a class="social" target="blank" href="https://www.instagram.com/tomasaxuuu/"><img src="../IMG/inst.png" width="25" height="25"></a>
+                        <a class="social" target="blank" href="https://vk.com/tomasaxuuu"><img src="../IMG/vk.png" width="25" height="25"></a>
+                        <a class="social" target="blank" href="https://open.spotify.com/user/5uh6ik7nbma6wbpaiq9ljr3i9"><img src="../IMG/spoty.png" width="25" height="25"></a>
+                    </div>
+                    <div class="url"><a href="../pages/Links.php">Links</a></div>
+                </div>
+        </header>
+    </div>
+    <main>
     <form action="../inc/edit.php" method="post">
         <h1>Редактирование</h1>
         <input type="hidden" name="id" value="<?= $product['id']?>">
@@ -26,5 +51,6 @@
         <input type="password" name="password" value="<?= $product['password']?>">
         <button type="submit">Изменить</button>
         </form>
+    </main>
 </body>
 </html>
