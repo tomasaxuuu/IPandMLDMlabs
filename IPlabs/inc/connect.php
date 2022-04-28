52917fd3b69a4ceefa331e54ea1ee431d5166227
Timur Mukhametzyanov <bratov.tima@mail.ru>
2022-04-29 00:05:12 +0400

    add Game

--- IPlabs/inc/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    $connect = mysqli_connect('localhost', 'root', '', 'db_users');
-    if(!$connect) {
-        die('error');
-    }
--- IPlabs/inc/delete.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    require_once './connect.php';
-
-    $id = $_GET['id'];
-    mysqli_query($connect, "DELETE FROM `users` WHERE `users`.`id` = '$id'");
-
-    header('Location: ../template/sql.php');
